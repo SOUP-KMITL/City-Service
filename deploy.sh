c22d0934bae1d2d5f7c38a46b07ec9f24846c32d
Kohpai <whcwhc78@gmail.com>
2018-01-29 20:37:04 +0700

    update external service address

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ -z "$2" ]
+then
+    echo "rsync -azrP --exclude-from \"rsync.exc\" --include \".dockerignore\" \
+        ./ api-smartCity:/home/centos/kohpai/CityService/"
+    rsync -azrP --exclude-from "rsync.exc" --include ".dockerignore" \
+        ./ api-smartCity:/home/centos/kohpai/CityService/
+else
+    echo "rsync -azrP --exclude-from \"rsync.exc\" --include \".dockerignore\" ./ $1:$2"
+    rsync -azrP --exclude-from "rsync.exc" --include ".dockerignore" ./ $1:$2
+fi
